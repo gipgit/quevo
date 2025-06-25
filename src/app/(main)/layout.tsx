0c40e77ddc67a8935d5b487af1796655f1339c5c
IPN-Agency <brentaquattordici@gmail.com>
2025-06-25 02:24:11 +0200

    Fix: Add React.ReactNode type to children prop in root layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-// app/layout.jsx (or .tsx)
+// app/layout.tsx
 
 import '@/app/styles/global.css'; 
 import '@/app/styles/landing.css'; 
@@ -7,26 +7,25 @@ import '@/app/styles/main.css';
 import Header from '@/components/layout/Header'; 
 import Footer from '@/components/layout/Footer'; 
 
+
 export const metadata = {
-  title: 'Queva App', // Adjust your app title
-  description: 'Your new Queva application', // Adjust your app description
+  title: 'Quevo App', 
+  description: 'Quevo', 
 };
 
 export default function RootLayout({
-  children, // The page content will be rendered here
+  children,
+}: {
+  children: React.ReactNode; 
 }) {
   return (
     <html lang="it">
       <body>
-        {/* The Header component for your entire application */}
         <Header />
-
-        {/* The 'children' prop renders the content of your pages (like app/page.jsx) */}
         <main>
             {children}
         </main>
 
-        {/* The Footer component for your entire application */}
         <Footer />
       </body>
     </html>
